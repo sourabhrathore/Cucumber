2fa03751c779c7de75d61682785fc23d5f9f41c3
sourabh rathore <sourabhrathore3110@gmail.com>
2022-05-17 17:39:44 +0530

    remove not required files

--- features/modules/first.rb
+++
@@ -1,9 +0,0 @@
-module First
-  def initialize
-    puts "first module intialize"
-  end
-
-  def method_one
-    puts "method_one of first module"
-  end
-end
--- features/modules/second.rb
+++
@@ -1,9 +0,0 @@
-module Second
-  def initialize
-    puts "second module intialize"
-  end
-
-  def method_one
-    puts "method_one of second module"
-  end
-end
